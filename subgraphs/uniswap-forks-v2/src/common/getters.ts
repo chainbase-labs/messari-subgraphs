ecafc38904e69db5f72da9c3bb9745dea78092cf
fishTsai20 <123794723+fishTsai20@users.noreply.github.com>
2023-07-21 10:43:16 +0800

    fix[uniswap-v2]:when token decimal not integer return default

--- subgraphs/uniswap-forks-v2/src/common/getters.ts
+++ subgraphs/uniswap-forks-v2/src/common/getters.ts
@@ -1,5 +1,5 @@
 // import { log } from "@graphprotocol/graph-ts";
-import { Address, ethereum } from "@graphprotocol/graph-ts";
+import { Address, BigInt, ethereum, log } from "@graphprotocol/graph-ts";
 import { NetworkConfigs } from "../../configurations/configure";
 import { TokenABI as ERC20 } from "../../generated/Factory/TokenABI";
 import { _Transfer, LiquidityPool, Token } from "../../generated/schema";
@@ -25,16 +25,20 @@ export function getOrCreateToken(
   event: ethereum.Event,
   address: string
 ): Token {
+  log.info("get or create token {}", [address]);
   let token = Token.load(address);
   if (!token) {
+    log.info("create token {}", [address]);
     token = new Token(address);
     const erc20Contract = ERC20.bind(Address.fromString(address));
-    const decimals = erc20Contract.try_decimals();
+
+    const decimals = fetchTokenDecimals(Address.fromString(address));
     // Using try_cause some values might be missing
     const name = erc20Contract.try_name();
     const symbol = erc20Contract.try_symbol();
     // TODO: add overrides for name and symbol
-    token.decimals = decimals.reverted ? DEFAULT_DECIMALS : decimals.value;
+
+    token.decimals = decimals;
     token.name = name.reverted ? "" : name.value;
     token.symbol = symbol.reverted ? "" : symbol.value;
     if (NetworkConfigs.getBrokenERC20Tokens().includes(address)) {
@@ -50,6 +54,24 @@ export function getOrCreateToken(
   }
 
   return token as Token;
+}
+
+export function fetchTokenDecimals(tokenAddress: Address): i32 {
+  // static definitions overrides
+  const contract = ERC20.bind(tokenAddress);
+  const res = contract.tryCall("decimals", "decimals():(uint8)", []);
+  if (res.reverted) {
+    return DEFAULT_DECIMALS;
+  }
+  const value = res.value;
+  const decimalU32 = value[0].toBigInt();
+  if (
+    decimalU32.ge(BigInt.fromI32(i32.MIN_VALUE)) &&
+    decimalU32.le(BigInt.fromI32(i32.MAX_VALUE))
+  ) {
+    return value[0].toI32();
+  }
+  return DEFAULT_DECIMALS;
 }
 
 export function getOrCreateLPToken(
